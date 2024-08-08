cac81cb7e67fd196157ca6c927def014e6352eff
Tyler Mays <16845057+TylerDM@users.noreply.github.com>
2024-08-08 17:04:29 -0400

    Add IEnumerable<Type>.WithAttribute<TAttribute>()

---
+++ source/TylerDM.StandardLibrary.Reflection/System/IEnumerableTypeExt.cs
@@ -0,0 +1,11 @@
+﻿namespace TylerDM.StandardLibrary.Reflection.System;
+
+public static class IEnumerableTypeExt
+{
+	public static IEnumerable<(Type Type, TAttribute Attribute)> WithAttribute<TAttribute>(this IEnumerable<Type> enumerable)
+		where TAttribute : Attribute =>
+		from type in enumerable
+		let attribute = type.GetCustomAttribute<TAttribute>()
+		where attribute is not null
+		select (type, attribute);
+}
