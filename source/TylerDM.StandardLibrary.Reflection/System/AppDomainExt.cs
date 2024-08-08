8eab99cc3e06b427bb34d7c9ac26c048b858b578
Tyler Mays <16845057+TylerDM@users.noreply.github.com>
2024-08-08 19:21:21 -0400

    Exclude MS assemblies, which more closely matches the method name, fixes an issue with EFC projects, and slightly improves performance in some scenarios.

--- source/TylerDM.StandardLibrary.Reflection/System/AppDomainExt.cs
+++ source/TylerDM.StandardLibrary.Reflection/System/AppDomainExt.cs
@@ -2,8 +2,13 @@
 
 public static class AppDomainExt
 {
+	#region fields
+	private static ConcurrentDictionary<AppDomain, Assembly[]> _assemblies = [];
+	#endregion
+
+	#region methods
 	public static IEnumerable<Type> GetImplementingTypes<T>(Func<Type, bool>? where = null) =>
-		AppDomain.CurrentDomain.GetImplementingTypes(typeof(T), where);
+	AppDomain.CurrentDomain.GetImplementingTypes(typeof(T), where);
 
 	public static IEnumerable<Type> GetImplementingTypes(Type type, Func<Type, bool>? where = null) =>
 		AppDomain.CurrentDomain.GetImplementingTypes(type, where);
@@ -24,5 +29,36 @@ public static class AppDomainExt
 	/// Returns all the types not created by the compiler.
 	/// </summary>
 	public static IEnumerable<Type> GetDeveloperTypes(this AppDomain appDomain, Func<Type, bool>? where = null) =>
-		appDomain.GetAssemblies().SelectMany(x => x.GetDeveloperTypes(where));
+		appDomain.getAssemblies().SelectMany(x => x.GetTypes());
+
+	public static bool IsMicrosoft(this Assembly assembly)
+	{
+		if (string.IsNullOrWhiteSpace(assembly.FullName)) return false;
+		return
+			assembly.FullName.StartsWith("System.") ||
+			assembly.FullName.StartsWith("Microsoft.") ||
+			assembly.FullName.StartsWith("netstandard") ||
+			assembly.FullName.StartsWith("mscorlib") ||
+			assembly.FullName.StartsWith("PresentationFramework") ||
+			assembly.FullName.StartsWith("WindowsBase") ||
+			assembly.FullName.StartsWith("PresentationCore") ||
+			assembly.FullName.Contains("PublicKeyToken=b77a5c561934e089");
+	}
+
+	public static void ClearAssemblyCache() =>
+		_assemblies = [];
+	#endregion
+
+	#region private methods
+	private static Assembly[] getAssemblies(this AppDomain appDomain) =>
+		_assemblies.GetOrAdd(appDomain, getAssembliesUncached);
+
+	private static Assembly[] getAssembliesUncached(AppDomain appDomain) =>
+		appDomain.GetAssemblies()
+			.Where(x =>
+				string.IsNullOrWhiteSpace(x.FullName) == false &&
+				x.IsMicrosoft() == false
+			)
+			.ToArray();
+	#endregion
 }
