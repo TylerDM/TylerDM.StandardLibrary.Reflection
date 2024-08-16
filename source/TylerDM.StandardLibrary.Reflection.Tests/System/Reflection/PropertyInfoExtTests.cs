37f4b2346077d8598ced46068816bffc114fdb51
Tyler Mays <16845057+TylerDM@users.noreply.github.com>
2024-08-16 15:26:08 -0400

    More new methods.

---
+++ source/TylerDM.StandardLibrary.Reflection.Tests/System/Reflection/PropertyInfoExtTests.cs
@@ -0,0 +1,39 @@
+﻿namespace TylerDM.StandardLibrary.Reflection.System.Reflection;
+
+public class PropertyInfoExtTests
+{
+	private static readonly int _expectedValue = Random.Shared.Next();
+
+	interface StaticInterface
+	{
+		static abstract int StaticProperty { get; }
+	}
+
+	class Implementation : StaticInterface
+	{
+		public static int StaticProperty => _expectedValue;
+		public int InstanceProperty { set { } }
+		public int WeirdStaticProperty { set { } }
+	}
+
+	[Fact]
+	public void IsStatic()
+	{
+		var type = typeof(Implementation);
+
+		var staticPropertyInfo = type.Get<PropertyInfo>(nameof(Implementation.StaticProperty));
+		Assert.True(staticPropertyInfo.IsStatic());
+
+		var instancePropertyInfo = type.Get<PropertyInfo>(nameof(Implementation.InstanceProperty));
+		Assert.False(instancePropertyInfo.IsStatic());
+	}
+
+	[Fact]
+	public void StaticGetValue()
+	{
+		var value = typeof(Implementation)
+			.Get<PropertyInfo>(nameof(StaticInterface.StaticProperty), x => x.IsStatic())
+			.StaticGetValue<int>();
+		Assert.Equal(_expectedValue, value);
+	}
+}
--- source/TylerDM.StandardLibrary.Reflection/System/Reflection/MethodInfoExt.cs
+++ source/TylerDM.StandardLibrary.Reflection/System/Reflection/MethodInfoExt.cs
@@ -2,14 +2,34 @@
 
 public static class MethodInfoExt
 {
-	public static async Task<object?> InvokeAsync(this MethodInfo method, object instance, params object[] args)
+	public static TValue Invoke<TValue>(this MethodInfo method, object instance, params object?[]? parameters) =>
+		(TValue)method.Invoke(instance, parameters)!;
+
+	public static TValue StaticInvoke<TValue>(this MethodInfo method, params object?[]? parameters) =>
+		(TValue)method.Invoke(null, parameters)!;
+
+	public static async Task<object?> InvokeAsync(this MethodInfo method, object instance, params object?[] args)
 	{
 		var task = method.Invoke(instance, args);
 		await (Task)task!;
 		return task.GetType()!.GetProperty(nameof(Task<object>.Result))!.GetValue(task, null);
 	}
 
-	public static async Task<TValue?> InvokeAsync<TValue>(this MethodInfo method, object instance, params object[] args)
+	public static async Task<object?> StaticInvokeAsync(this MethodInfo method, params object?[] args)
+	{
+		var task = method.Invoke(null, args);
+		await (Task)task!;
+		return task.GetType()!.GetProperty(nameof(Task<object>.Result))!.GetValue(task, null);
+	}
+
+	public static async Task<TValue?> StaticInvokeAsync<TValue>(this MethodInfo method, params object?[] args)
+	{
+		var task = method.Invoke(null, args);
+		await (Task)task!;
+		return (TValue?)task.GetType()!.GetProperty(nameof(Task<object>.Result))!.GetValue(task, null);
+	}
+
+	public static async Task<TValue?> InvokeAsync<TValue>(this MethodInfo method, object instance, params object?[] args)
 	{
 		var task = method.Invoke(instance, args);
 		await (Task)task!;
